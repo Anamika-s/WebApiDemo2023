a174296ea32eed1be36199a0758c3017fdfc62df
Anamika-s <anamika.sawhney22@gmail.com>
2023-06-20 10:46:46 +0530

    Add project files.

---
+++ WebApiDemo/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            List<Student> students = new List<Student>()
+                {
+                 new Student{ Id = 1,Name="ajay"},
+                 new Student{ Id = 2,Name="ajay"}
+            };
+            return Ok(students);
+        
+        }
+        
+    }
+}
---
+++ WebApplication1/Controllers/HomeController1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        // GET: HomeController1
+        public async Task< ActionResult> Index()
+        {
+             using (var client = new HttpClient())
+                {
+                    //Send HTTP requests from here. 
+                    client.BaseAddress = new Uri("https://localhost:44322/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    //GET Method  
+                    HttpResponseMessage response = await client.GetAsync("api/Students");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var jsonString = response.Content.ReadAsStringAsync();
+                        jsonString.Wait();
+                        var student = JsonConvert.DeserializeObject<List<Supplier>>(jsonString.Result);
+
+
+
+
+                        return View(student);
+                    }
+                    else
+                        return View(null);
+                }
+            }
+
+        
+        // GET: HomeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
